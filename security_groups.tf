3f33f13a9e87d3c087b9f29ac15a1bddc1015dac
tbriggs1 <tombriggs1993@outlook.com>
2022-09-14 08:05:35 +0100

    adding security groups

---
+++ security_groups.tf
@@ -0,0 +1,98 @@
+# Create SG for LB, only TPC/80, TCP/433 and outbound access
+resource "aws_security_group" "lb-sg" {
+  provider    = aws.region-master
+  name        = "lb-sg"
+  description = "Allow 443 and traffic to Jenkins SG"
+  vpc_id      = aws_vpc.vpc_master.id
+
+  ingress {
+    description = "Allow 443 from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow 80 from anywhere for redirection"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "jenkins-sg" {
+  provider    = aws.region-master
+  name        = "jenkins-sg"
+  description = "Allow 22  and 8080"
+  vpc_id      = aws_vpc.vpc_master.id
+
+  ingress {
+    description = "Allow 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description     = "Allow 8080 from anywhere"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb-sg.id]
+  }
+
+  ingress {
+    description = "Allow traffic from eu-west-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "jenkins-worker-sg" {
+  provider    = aws.region-worker
+  name        = "jenkins-worker-sg"
+  description = "Allow 22  and 8080 for worker"
+  vpc_id      = aws_vpc.vpc_worker.id
+
+  ingress {
+    description = "Allow 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow traffic from eu-west-2"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["172.16.1.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -36,7 +36,7 @@ resource "aws_internet_gateway" "master-gw" {
 
 resource "aws_internet_gateway" "worker-gw" {
   provider = aws.region-worker
-  vpc_id = aws_vpc.vpc_worker.id
+  vpc_id   = aws_vpc.vpc_worker.id
 
   tags = {
     Name = "worker"
@@ -71,3 +71,72 @@ resource "aws_subnet" "subnet_3" {
   cidr_block = "10.0.1.0/24"
 }
 
+# Peering connection request from eu-west-2
+resource "aws_vpc_peering_connection" "euwest2-euwest1" {
+  provider    = aws.region-master
+  peer_vpc_id = aws_vpc.vpc_worker.id
+  vpc_id      = aws_vpc.vpc_master.id
+  peer_region = var.region-worker
+}
+
+# Accept VPC peering in eu-west-1 from eu-west-2
+resource "aws_vpc_peering_connection_accepter" "accept_peering" {
+  provider                  = aws.region-worker
+  vpc_peering_connection_id = aws_vpc_peering_connection.euwest2-euwest1.id
+  auto_accept               = true
+}
+
+# Create route table in eu-west-2
+resource "aws_route_table" "internet_route" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.master-gw.id
+  }
+  route {
+    cidr_block                = "10.0.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.euwest2-euwest1.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Master-Region-RT"
+  }
+}
+
+#Overwrite default route table
+resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
+  provider       = aws.region-master
+  vpc_id         = aws_vpc.vpc_master.id
+  route_table_id = aws_route_table.internet_route.id
+}
+
+
+# Create route table in eu-west-2
+resource "aws_route_table" "internet_route_worker" {
+  provider = aws.region-worker
+  vpc_id   = aws_vpc.vpc_worker.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.worker-gw.id
+  }
+  route {
+    cidr_block                = "172.16.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.euwest2-euwest1.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Worker-Region-RT"
+  }
+}
+
+#Overwrite default route table
+resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
+  provider       = aws.region-worker
+  vpc_id         = aws_vpc.vpc_worker.id
+  route_table_id = aws_route_table.internet_route_worker.id
+}
